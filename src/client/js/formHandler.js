64610521bfbb243a9209fa1136e9bd82eb6858c5
hulyak <57311842+hulya95@users.noreply.github.com>
2020-04-15 16:56:29 -0700

    typo  fix

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -15,31 +15,32 @@ function handleSubmit(event) {
         });
         return true;
     }
-    const postData = async (url = '', data = {}) => {
-        const response = await fetch(url, {
-            method: 'POST',
-            credentials: 'same-origin',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data) // body data type must match "Content-Type" header
-        });
-
-        try {
-            const someData = await response.json();
-            return postData;
-        } catch (error) {
-            console.log('error', error);
-        }
-    };
-    const updateUI = async (input) => {
-        document.getElementById('polarity').innerHTML = input.polarity;
-        document.getElementById('polConfidence').innerHTML = input.polConfidence;
-        document.getElementById('subjectivity').innerHTML = input.subjectivity;
-        document.getElementById('subConfidence').innerHTML = input.subConfidence;
-        document.getElementById('text').innerHTML = input.text;
-    };
 }
+const postData = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+
+    try {
+        const someData = await response.json();
+        return postData;
+    } catch (error) {
+        console.log('error', error);
+    }
+}
+const updateUI = async (input) => {
+    document.getElementById('polarity').innerHTML = input.polarity;
+    document.getElementById('polConfidence').innerHTML = input.polConfidence;
+    document.getElementById('subjectivity').innerHTML = input.subjectivity;
+    document.getElementById('subConfidence').innerHTML = input.subConfidence;
+    document.getElementById('text').innerHTML = input.text;
+};
+
 
 export {
     handleSubmit
